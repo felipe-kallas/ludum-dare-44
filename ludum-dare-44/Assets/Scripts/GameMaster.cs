0dd8c30d401db2b8be45cda33623e67515519379
Felipe Kallás Silva <felipe.kallas@gmail.com>
2019-04-27 07:47:09 -0300

    first build
    > Player can move to the sides and jump
    > Basic Stage with 1 Platform
    > Player respawn funcionality implemented on GM
    > Camera snaps back to player after respawn

---
+++ ludum-dare-44/Assets/Scripts/GameMaster.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour
+{
+    public static GameMaster gm;
+
+    void Start () {
+        if (gm == null) {
+            gm = GameObject.FindGameObjectWithTag ("GM").GetComponent <GameMaster> ();
+        }
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+    public IEnumerator RespawnPlayer () {
+        yield return new WaitForSeconds(spawnDelay);
+    
+        // TODO: Spawn animation/particles
+        Instantiate (playerPrefab, spawnPoint.position, spawnPoint.rotation);
+    }
+
+
+    public static void KillPlayer (Player player) {
+        Destroy (player.gameObject);
+        gm.StartCoroutine (gm.RespawnPlayer ());
+    }
+}
---
+++ ludum-dare-44/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[System.Serializable]
+	public class PlayerStats {
+		public int health = 5;
+	}
+
+	public PlayerStats playerStats = new PlayerStats ();
+	public int fallBoundary = -10;
+
+	public void DamagePlayer (int damage) {
+		playerStats.health -= damage;
+		if (playerStats.health <= 0) {
+			GameMaster.KillPlayer (this);
+		}
+	}
+
+	void Update () {
+		if (transform.position.y <= fallBoundary) {
+			DamagePlayer (playerStats.health);
+		}
+	}
+}
